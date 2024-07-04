307c79241d3c060ecf5f4dc1663f49b7c2dcc912
Mike <alisonshu18@gmail.com>
2024-07-04 12:44:29 +0900

    add a global alert

--- nonio-ios/Sources/Features/nonio_iosApp.swift
+++ nonio-ios/Sources/Features/nonio_iosApp.swift
@@ -5,16 +5,22 @@ struct nonio_iosApp: App {
     private let appSettings = AppSettings()
     private let votingService = UserVotingService()
     private let notificationDataTicker = NotificationUnreadTicker()
+    @StateObject var alertInteractor = GlobalAlertObject()
+
     var body: some Scene {
         WindowGroup {
             MainView()
                 .environmentObject(appSettings)
                 .environmentObject(notificationDataTicker)
                 .environmentObject(votingService)
+                .environmentObject(alertInteractor)
                 .onLoad {
                     if appSettings.hasLoggedIn {
                         votingService.fetchVotes()
                     }
+                }
+                .alert(isPresented: $alertInteractor.isShowingAlert) {
+                    alertInteractor.alert ?? Alert(title: Text(""))
                 }
         }
     }
